ac78d377225182504460d1f330a0cb272d4aecf4
Alex Urbini <aurbini92@gmail.com>
2020-03-12 23:41:01 -0400

    get, post user routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Books from "./pages/Books";
+import Login from "./pages/Login"
+import Nav from "./components/Nav";
+import Search from "./pages/Search";
+import Friends from "./pages/Friends"; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/","/login"]}>
+            <Login/>
+          </Route>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+          <Route exact path="/friends">
+            <Friends />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
