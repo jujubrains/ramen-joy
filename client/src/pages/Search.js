c79dce3db0828df2cf876931753c20e06968fa3b
Alex Urbini <aurbini92@gmail.com>
2020-03-13 13:59:11 -0400

    home page linked

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,13 @@
 import React, {useContext, useEffect, useState} from 'react';
 import axios from "axios"; 
+import MediaCard from "../components/Card";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 
 const Search = () => {
   const [search, setSearch] = useState(); 
-  const [searchResults, setSearchResults] = useState(); 
+  const [searchResults, setSearchResults] = useState([]); 
 
   function handleInput(e){
     const input = e.target.value; 
@@ -16,9 +19,24 @@ const Search = () => {
     console.log("sending search to api"); 
     console.log(search); 
     const response = await axios("/api/restaurant/yelp")
-    console.log(response.data); 
-    
+    const {data} = response; 
+    console.log(data); 
+    setSearchResults(data);
+    renderRestaurants(); 
 }
+  function renderRestaurants(){
+
+    return(
+      <Grid item sm={6} xs={12} spacing={3}>
+        {
+        searchResults.map((restaurant) =>{
+          const { image_url, name} = restaurant; 
+          return <MediaCard imageUrl={image_url} name={name}/>
+        })
+      }
+      </Grid>
+    ) 
+  }
 
   return ( 
     <div>
@@ -31,7 +49,8 @@ const Search = () => {
         />
         <button type="submit" >Search</button>
       </form> 
-      <div>Display Search results</div>
+      <div>Display Search results
+      </div>
     </div>
    );
 }
