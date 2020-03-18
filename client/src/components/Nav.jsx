61248fb4d84b1f58fad51a14051a3cf8f65d678a
Sonechka M <s212288s@gmail.com>
2020-03-18 10:23:36 -0400

    fixed navabar spacing and margin-top

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,7 +8,7 @@ const Nav = () => {
   const [state, dispatch] = useTodoContext();
   console.log(state); 
   return (
-    <div>
+    <div className="nav-wrapper">
       <nav class="navbar navbar-expand-lg navbar-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
