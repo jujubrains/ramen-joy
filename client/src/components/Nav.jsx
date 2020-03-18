93a9510eecc8aa3afa712f57b3ef55aa5433b789
Sonechka M <s212288s@gmail.com>
2020-03-18 15:57:14 -0400

    nav links

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom"; 
 import "../style/Nav.css";
 import {useTodoContext} from "../utils/GlobalState";
@@ -6,61 +6,45 @@ import {useTodoContext} from "../utils/GlobalState";
 
 const Nav = () => {
   const [state, dispatch] = useTodoContext();
-  console.log(state); 
+  console.log(state);
+  const [menuOpen, setMenuOpen] = useState(false)
+  const handleMenuOpen = () => {
+    setMenuOpen(!menuOpen)
+  }
   return (
     <div className="nav-wrapper">
       <nav class="navbar navbar-expand-lg navbar-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button type="button" class="navbar-toggler" onClick={handleMenuOpen}>
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class={menuOpen ? "navbar-collapse show" : "navbar-collapse collapse"} id="navbarNav">
           <ul class="navbar-nav">
-            <Link to ="/home">
             <li class="nav-item">
-              <a class="nav-link">Home</a>
+              <Link to ="/home" class="nav-link" onClick={() => setMenuOpen(false)}>Home</Link>
             </li>
-            </Link>
-            <Link to ="/search">
-              <li class="nav-item">
-                <a class="nav-link">Restaurants</a>
-              </li>
-            </Link>
-            <Link to ="/users">
-              <li class="nav-item">
-                <a class="nav-link">Ramenuers</a>
-              </li>
-            </Link>
-            <Link to ="/friends">
-              <li class="nav-item">
-                <a class="nav-link">Friends</a>
-              </li>
-            </Link>
-            <Link to ="/vids">
-              <li class="nav-item">
-                <a class="nav-link">Videos</a>
-              </li>
-            </Link>
-            <Link to ="/info">
-              <li class="nav-item">
-                <a class="nav-link">Facts</a>
-              </li>
-            </Link>
-            <Link to ="/register">
-              <li class="nav-item">
-                <a class="nav-link">Register</a>
-              </li>
-            </Link>
+            <li class="nav-item">
+              <Link to ="/search" class="nav-link" onClick={() => setMenuOpen(false)}>Restaurants</Link>
+            </li>
+            <li class="nav-item">
+              <Link to ="/users" class="nav-link" onClick={() => setMenuOpen(false)}>Rameneurs</Link>
+            </li>
+            <li class="nav-item">
+              <Link to ="/friends" class="nav-link" onClick={() => setMenuOpen(false)}>Friends</Link>
+            </li>
+            <li class="nav-item">
+              <Link to ="/vids" class="nav-link" onClick={() => setMenuOpen(false)}>Videos</Link>
+            </li>
+            <li class="nav-item">
+              <Link to ="/info" class="nav-link" onClick={() => setMenuOpen(false)}>Facts</Link>
+            </li>
+            <li class="nav-item">
+              <Link to ="/register" class="nav-link" onClick={() => setMenuOpen(false)}>Register</Link>
+            </li>
             {!localStorage.getItem("id")?  
-           <Link to ="/login">
            <li class="nav-item">
-             <a class="nav-link">Login</a>
-           </li>
-            </Link> : 
-            <Link to ="/logout">
-              <li class="nav-item">
-                <a class="nav-link">Logout</a>
-              </li>
-            </Link>}
+           <Link to ="/login" class="nav-link" onClick={() => setMenuOpen(false)}>Login</Link></li> : 
+            <li class="nav-item">
+            <Link to ="/home" class="nav-link" onClick={() => setMenuOpen(false)}>Logout</Link></li>}
           </ul>
         </div>
       </nav>
